0dbd8c098a684351c97648a30437d6637ab225c2
Ethan Chen <intervigil@gmail.com>
2013-10-28 18:00:23 -0700

    m7att: remove GSM APN list
    Change-Id: I020ca744c5604e13a237d394cffa667d09a9e4d8

--- cm.mk
+++ cm.mk
@@ -1,7 +1,5 @@
 $(call inherit-product, device/htc/m7att/full_m7att.mk)
 
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
